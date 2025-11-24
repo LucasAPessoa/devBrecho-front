cc205f6a5dad7089fcd40563edeb376c4576397f
LucasAPessoa <lucasapessoa@outlook.com>
2025-11-23 23:54:05 -0300

    feat: add response interceptors to handle API errors and log messages

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,3 +5,23 @@ const baseURL = import.meta.env.VITE_API_BASE_URL;
 export const api = axios.create({
     baseURL,
 });
+
+api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response) {
+            const { status } = error.response;
+
+            if (status === 401) {
+                console.warn("Sessão expirada");
+                //TODO:Redireccionar a login
+            }
+
+            if (status === 500) {
+                console.error("Servidor explodiu.");
+            }
+        }
+
+        return Promise.reject(error);
+    }
+);
