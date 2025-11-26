d7871445150bc4d7a7b2d1ea6f500729d9b6f456
LucasAPessoa <lucasapessoa@outlook.com>
2025-11-26 15:34:29 -0300

    feat: add SearchBar component

---
+++ src/shared/ui/Input/SearchBar.tsx
@@ -0,0 +1,36 @@
+import { Input, InputGroup, InputRightElement } from "@chakra-ui/react";
+import { SearchIcon } from "@chakra-ui/icons";
+
+interface SearchBarProps {
+    value: string;
+    onChange: (newValue: string) => void;
+    onSearch: () => void;
+}
+
+export function SearchBar({ value, onChange, onSearch }: SearchBarProps) {
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === "Enter") {
+            onSearch();
+        }
+    };
+
+    return (
+        <InputGroup>
+            <Input
+                type="text"
+                placeholder="Pesquise algo..."
+                variant="outline"
+                _focus={{ borderColor: "blue.400" }}
+                value={value}
+                onChange={(e) => onChange(e.target.value)}
+                onKeyDown={handleKeyDown}
+            />
+
+            <InputRightElement
+                pointerEvents="none"
+                children={<SearchIcon color="gray.300" />}
+                onClick={onSearch}
+            />
+        </InputGroup>
+    );
+}
