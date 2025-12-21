02e5101a004e7e2b7b80ade26081b05830d3fcf2
LucasAPessoa <lucasapessoa@outlook.com>
2025-12-21 12:34:57 -0300

    feat: update routing to include Login component and redirect to home

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom"; // Adicionei Navigate
 import { Layout } from "./shared/ui/Navigation/Layout";
 import { Setores } from "./pages/Setores";
 import { Fornecedoras } from "./pages/Fornecedoras";
 import { Bolsas } from "./pages/Bolsas";
 import { Prazos } from "./pages/Prazos";
+import { Login } from "./pages/Login";
 
 function App() {
     return (
         <Routes>
-            <Route path="/" element={<Layout />}>
-                <Route index element={<h1>Bem-vindo ao DevBrecho!</h1>} />
+            <Route path="/" element={<Login />} />
+
+            <Route path="/login" element={<Login />} />
+
+            <Route element={<Layout />}>
+                <Route
+                    path="/dashboard"
+                    element={<h1>Bem-vindo ao DevBrecho!</h1>}
+                />
                 <Route path="/setores" element={<Setores />} />
                 <Route path="/fornecedoras" element={<Fornecedoras />} />
                 <Route path="/bolsas" element={<Bolsas />} />
                 <Route path="/prazos" element={<Prazos />} />
             </Route>
+
+            <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
     );
 }
